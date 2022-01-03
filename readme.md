b9cbc655ee214d0b224b57285e8d6ceff63a79bb
Henrique Lopes <henrique.l.vieira92@gmail.com>
2022-01-03 17:35:19 -0300

    Add files via upload

--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ tsc --init
 
 npm install --save-dev typescript
 npm install --save-dev @types/node 
+npm install --save express
+npm install --save-dev @types/express
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World")
+import express, {Request, Response, NextFunction} from "express";
+
+const APP = express();
+const PORT = 3000 || 3333;
+
+APP.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    console.log('Chamada da rota /status')
+    res.status(200).send({foo: 'bar'}); //Responder a requisição c/ o Status 200
+
+});
+
+APP.listen(PORT, () => {
+    console.log('Aplicação em execução na porta ' + PORT)    
+});
+
+ 

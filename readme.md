a397614b018cc58a6c10400c697311d2fa0e87ce
Henrique Lopes <henrique.l.vieira92@gmail.com>
2022-01-04 11:18:27 -0300

    Add files via upload

--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
-
-
 #Iniciar o TypeScript
 tsc --init
 
@@ -7,3 +5,6 @@ npm install --save-dev typescript
 npm install --save-dev @types/node 
 npm install --save express
 npm install --save-dev @types/express
+npm install --save-dev ts-node-dev
+npm install --save http-status-codes
+
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import express, {Request, Response, NextFunction} from "express";
+import {StatusCodes} from 'http-status-codes';
 
-const APP = express();
+import usersRoute from "./routes/user_route";
+import statusRoute from "./routes/status_route";
+
+const APP = express(); //Instanciar o Express
 const PORT = 3000 || 3333;
 
-APP.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    console.log('Chamada da rota /status')
-    res.status(200).send({foo: 'bar'}); //Responder a requisição c/ o Status 200
+//Configuração da aplicação:
+APP.use(express.json()); //Middleware p/ lidar c/ o JSON no Content-Type
+APP.use(express.urlencoded( {extended: true} )); //Middleware p/ realizar o parsing do conteúdo das requisições
 
-});
 
+//Configuração das Rotas:
+APP.use(usersRoute); //Adição das rotas de Usuário
+APP.use(statusRoute); //Adição das rotas de Status
+
+
+//Inicialização do Servidor:
 APP.listen(PORT, () => {
     console.log('Aplicação em execução na porta ' + PORT)    
 });

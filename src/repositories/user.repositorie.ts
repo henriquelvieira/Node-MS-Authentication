e09be31c6ee28c786f03e1152e8ac994db89d508
Henrique Lopes <henrique.l.vieira92@gmail.com>
2022-01-05 08:26:53 -0300

    Add files via upload

--- src/repositories/user.repositorie.ts
+++ src/repositories/user.repositorie.ts
@@ -16,14 +16,14 @@ class UserRepository {
     async findUserById(uuid: string): Promise<User[]>{
         try {
             const query = `SELECT UUID,
-                                USERNAME                         
-                            FROM APPLICATION_USER
+                                  USERNAME                         
+                             FROM APPLICATION_USER
                             WHERE UUID = $1`;
             const values = [uuid];
 
             const { rows } = await db.query<User>(query, values);
-            const user = rows;
-            return rows;
+            const [user] = rows;
+            return [user];
         } catch (error) {
             throw new DatabaseError('Erro na consulta por ID', error);
         };
@@ -45,11 +45,36 @@ class UserRepository {
             const { rows } = await db.query<{ uuid: string }>(script, values);
             const [newUser] = rows;
             return newUser.uuid;
-
         } catch (error) {
             throw new DatabaseError('Erro na consulta por ID', error);
         };
    };
+
+    async update(user: User): Promise<void> {
+        try {
+            const script  = `UPDATE APPLICATION_USER
+                                SET USERNAME = $1
+                                    PASSWORD = crypt($2, 'my_salt')
+                            WHERE UUID = $3`;
+            const values = [user.username, user.password, user.uuid];
+
+            await db.query(script, values);            
+        } catch (error) {
+            throw new DatabaseError('Erro na consulta por ID', error);
+        };
+   };
+
+   async delete(uuid: string): Promise<void> {
+        try {
+            const script  = `DELETE APPLICATION_USER WHERE UUID = $1`;
+            const values = [uuid];
+
+            await db.query(script, values);            
+        } catch (error) {
+            throw new DatabaseError('Erro na consulta por ID', error);
+        };
+   };   
+
 
 };
 

cdf5c03513eb5977ae556a3d7b755c7c129eff8e
Henrique Lopes <henrique.l.vieira92@gmail.com>
2022-02-09 14:39:43 -0300

    Fix ESlint Warnings
    Adição de regra para ignorar a validação de Lint para no-unused-vars para o caso do Next
    Desabilitar no-explicit-any para os casos do tipo Error

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -4,7 +4,8 @@ import logger from '../logger';
 import DatabaseError from '../models/errors/database.error.model';
 import ForbiddenError from '../models/errors/forbidden.error.model';
 
-function errorHandlerMiddleware(error: any, req: Request, res: Response, next : NextFunction) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function errorHandlerMiddleware(error: any, req: Request, res: Response, _next : NextFunction) {
     
     const errorMessage: string = error.message || 'unexpected-error';
     let errorCode = StatusCodes.INTERNAL_SERVER_ERROR;
